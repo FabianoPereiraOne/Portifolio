e82d51fb4d5436cb275e7f83df4a266b85325192
FabianoPereira <fabianogamer170@gmail.com>
2020-11-09 16:34:58 -0300

    ajust margin

--- public/js/animation.js
+++ public/js/animation.js
@@ -1,3 +1,16 @@
+(function($){
+   "use strict";
+
+   //Inicializar contador
+   var $doc = $('html,body');
+   $('.scroll-page').click(function(){
+       $doc.animate({
+           scrollTop:$($.attr(this,'href')).offset().top
+       },500);
+       return false;
+   })
+})(jQuery);
+
 alegre = () =>{
    const logo = document.querySelector("#logo") 
    logo.classList.toggle("wavy")
